72a0794e18b82fbf1b59999858669732a444f5b9
Alin Panaitiu <alin.p32@gmail.com>
2017-11-30 23:48:40 +0200

    Initial Commit

---
+++ Adaptivo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Adaptivo
+//
+//  Created by Alin on 30/11/2017.
+//  Copyright © 2017 Alin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Adaptivo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Adaptivo
+//
+//  Created by Alin on 30/11/2017.
+//  Copyright © 2017 Alin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
