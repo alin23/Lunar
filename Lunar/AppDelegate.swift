e53933643df0d6a65d2610a927434e1407823ca3
Alin Panaitiu <alin.p32@gmail.com>
2018-06-29 20:27:34 +0300

    Add Sentry

--- Lunar/AppDelegate.swift
+++ Lunar/AppDelegate.swift
@@ -9,6 +9,7 @@
 import Cocoa
 import CoreLocation
 import HotKey
+import Sentry
 import ServiceManagement
 import SwiftyBeaver
 import WAYWindow
@@ -61,6 +62,20 @@ class AppDelegate: NSObject, NSApplicationDelegate, CLLocationManagerDelegate, N
     @IBOutlet var toggleMenuItem: NSMenuItem!
 
     let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+
+    func application(
+        application _: NSApplication,
+        didFinishLaunchingWithOptions _: [NSObject: AnyObject]?
+    ) -> Bool {
+        do {
+            Client.shared = try Client(dsn: "***REMOVED***")
+            try Client.shared?.startCrashHandler()
+        } catch let error {
+            log.error(error)
+        }
+
+        return true
+    }
 
     func menuWillOpen(_: NSMenu) {
         toggleMenuItem.title = AppDelegate.getToggleMenuItemTitle()
