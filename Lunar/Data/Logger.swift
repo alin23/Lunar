6951722fd46864bb41cd44e8cec6f8ad1cda8fec
Alin Panaitiu <alin.p32@gmail.com>
2018-07-07 13:40:01 +0300

    Fallback to generated names, and add more logging

---
+++ Lunar/Data/Logger.swift
@@ -0,0 +1,85 @@
+//
+//  Logger.swift
+//  Lunar
+//
+//  Created by Alin on 07/07/2018.
+//  Copyright © 2018 Alin. All rights reserved.
+//
+
+import Crashlytics
+import Foundation
+import SwiftyBeaver
+
+class Logger: SwiftyBeaver {
+    class func initLogger() {
+        let console = ConsoleDestination()
+        let file = FileDestination()
+
+        Logger.addDestination(console)
+        Logger.addDestination(file)
+    }
+
+    open override class func verbose(
+        _ message: @autoclosure () -> Any,
+        _
+        file: String = #file,
+        _ function: String = #function,
+        line: Int = #line,
+        context: Any? = nil
+    ) {
+        super.verbose(message, file, function, line: line, context: context)
+        crashlog(String(describing: message()))
+    }
+
+    open override class func debug(
+        _ message: @autoclosure () -> Any,
+        _
+        file: String = #file,
+        _ function: String = #function,
+        line: Int = #line,
+        context: Any? = nil
+    ) {
+        super.debug(message, file, function, line: line, context: context)
+        crashlog(String(describing: message()))
+    }
+
+    open override class func info(
+        _ message: @autoclosure () -> Any,
+        _
+        file: String = #file,
+        _ function: String = #function,
+        line: Int = #line,
+        context: Any? = nil
+    ) {
+        super.info(message, file, function, line: line, context: context)
+        crashlog(String(describing: message()))
+    }
+
+    open override class func warning(
+        _ message: @autoclosure () -> Any,
+        _
+        file: String = #file,
+        _ function: String = #function,
+        line: Int = #line,
+        context: Any? = nil
+    ) {
+        super.warning(message, file, function, line: line, context: context)
+        crashlog(String(describing: message()))
+    }
+
+    open override class func error(
+        _ message: @autoclosure () -> Any,
+        _
+        file: String = #file,
+        _ function: String = #function,
+        line: Int = #line,
+        context: Any? = nil
+    ) {
+        super.error(message, file, function, line: line, context: context)
+        crashlog(String(describing: message()))
+    }
+
+    class func crashlog(_ message: String) {
+        CLSLogv("%@", getVaList([message]))
+    }
+}
