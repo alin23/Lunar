e6aee2e05cf50c9678e102688e7e882fb6f8de07
Alin Panaitiu <alin.p32@gmail.com>
2018-07-14 14:15:44 +0300

    Update to SwiftDate 5

--- Lunar/Data/Moment.swift
+++ Lunar/Data/Moment.swift
@@ -26,49 +26,65 @@ class Moment: NSObject, NSCoding {
     static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
     static let ArchiveURL = DocumentsDirectory.appendingPathComponent("moments")
 
-    init(_ solar: Solar) {
-        let sevenAM = DateInRegion().startOf(component: .day).atTime(hour: 7, minute: 0, second: 0)?.absoluteDate
-        let noon = DateInRegion().startOf(component: .day).atTime(hour: 12, minute: 0, second: 0)?.absoluteDate
-        let sevenPM = DateInRegion().startOf(component: .day).atTime(hour: 19, minute: 0, second: 0)?.absoluteDate
+    static func defaultMoments() -> (DateInRegion, DateInRegion, DateInRegion) {
+        let sevenAM = DateInRegion().convertTo(region: Region.local).dateBySet(hour: 7, min: 0, secs: 0)!
+        let noon = DateInRegion().convertTo(region: Region.local).dateBySet(hour: 12, min: 0, secs: 0)!
+        let sevenPM = DateInRegion().convertTo(region: Region.local).dateBySet(hour: 19, min: 0, secs: 0)!
 
-        sunrise = DateInRegion(absoluteDate: solar.sunrise ?? sevenAM!, in: Region.Local())
-        sunset = DateInRegion(absoluteDate: solar.sunset ?? sevenPM!, in: Region.Local())
-        solarNoon = DateInRegion(absoluteDate: solar.solarNoon ?? noon!, in: Region.Local())
-        dayLength = UInt64(solar.sunset! - solar.sunset!)
-        civilSunrise = DateInRegion(absoluteDate: solar.civilSunrise ?? sevenAM!, in: Region.Local())
-        civilSunset = DateInRegion(absoluteDate: solar.civilSunset ?? sevenPM!, in: Region.Local())
-        nauticalSunrise = DateInRegion(absoluteDate: solar.nauticalSunrise ?? sevenAM!, in: Region.Local())
-        nauticalSunset = DateInRegion(absoluteDate: solar.nauticalSunset ?? sevenPM!, in: Region.Local())
-        astronomicalSunrise = DateInRegion(absoluteDate: solar.astronomicalSunrise ?? sevenAM!, in: Region.Local())
-        astronomicalSunset = DateInRegion(absoluteDate: solar.astronomicalSunset ?? sevenPM!, in: Region.Local())
+        return (sevenAM, noon, sevenPM)
+    }
+
+    static func defaultMomentsAsDates() -> (Date, Date, Date) {
+        let (sevenAM, noon, sevenPM) = defaultMoments()
+        return (
+            sevenAM.date,
+            noon.date,
+            sevenPM.date
+        )
+    }
+
+    init(_ solar: Solar) {
+        let (sevenAM, noon, sevenPM) = Moment.defaultMomentsAsDates()
+
+        sunrise = DateInRegion(solar.sunrise ?? sevenAM, region: Region.local)
+        sunset = DateInRegion(solar.sunset ?? sevenPM, region: Region.local)
+        solarNoon = DateInRegion(solar.solarNoon ?? noon, region: Region.local)
+        dayLength = UInt64((sunset - sunset).timeInterval)
+        civilSunrise = DateInRegion(solar.civilSunrise ?? sevenAM, region: Region.local)
+        civilSunset = DateInRegion(solar.civilSunset ?? sevenPM, region: Region.local)
+        nauticalSunrise = DateInRegion(solar.nauticalSunrise ?? sevenAM, region: Region.local)
+        nauticalSunset = DateInRegion(solar.nauticalSunset ?? sevenPM, region: Region.local)
+        astronomicalSunrise = DateInRegion(solar.astronomicalSunrise ?? sevenAM, region: Region.local)
+        astronomicalSunset = DateInRegion(solar.astronomicalSunset ?? sevenPM, region: Region.local)
     }
 
     init(result: [String: JSON]) {
-        let localTime = { (key: String) in DateInRegion(
-            string: result[key]!.stringValue,
-            format: .iso8601(options: .withInternetDateTime)
-        )!.toRegion(Region.Local())
+        let (sevenAM, noon, sevenPM) = Moment.defaultMoments()
+
+        let localTime = { (key: String) -> DateInRegion? in
+            let strDate = result[key]?.stringValue
+            return strDate?.toDate()?.convertTo(region: Region.local)
         }
 
-        sunrise = localTime("sunrise")
-        sunset = localTime("sunset")
-        solarNoon = localTime("solar_noon")
+        sunrise = localTime("sunrise") ?? sevenAM
+        sunset = localTime("sunset") ?? sevenPM
+        solarNoon = localTime("solar_noon") ?? noon
         dayLength = result["day_length"]!.uInt64Value
-        civilSunrise = localTime("civil_twilight_begin")
-        civilSunset = localTime("civil_twilight_end")
-        nauticalSunrise = localTime("nautical_twilight_begin")
-        nauticalSunset = localTime("nautical_twilight_end")
-        astronomicalSunrise = localTime("astronomical_twilight_begin")
-        astronomicalSunset = localTime("astronomical_twilight_end")
+        civilSunrise = localTime("civil_twilight_begin") ?? sevenAM
+        civilSunset = localTime("civil_twilight_end") ?? sevenPM
+        nauticalSunrise = localTime("nautical_twilight_begin") ?? sevenAM
+        nauticalSunset = localTime("nautical_twilight_end") ?? sevenPM
+        astronomicalSunrise = localTime("astronomical_twilight_begin") ?? sevenAM
+        astronomicalSunset = localTime("astronomical_twilight_end") ?? sevenPM
     }
 
     // MARK: UserDefaults
 
     init?(defaults: UserDefaults = datastore.defaults) {
-        let localTime = { (iso: String) in DateInRegion(
-            string: iso,
-            format: .iso8601Auto
-        )!.toRegion(Region.Local())
+        let (sevenAM, noon, sevenPM) = Moment.defaultMoments()
+
+        let localTime = { (iso: String) -> DateInRegion? in
+            iso.toDate()?.convertTo(region: Region.local)
         }
 
         guard let sunrise = defaults.string(forKey: "sunrise"),
@@ -84,51 +100,51 @@ class Moment: NSObject, NSCoding {
             return nil
         }
 
-        self.sunrise = localTime(sunrise)
-        self.sunset = localTime(sunset)
-        self.solarNoon = localTime(solarNoon)
+        self.sunrise = localTime(sunrise) ?? sevenAM
+        self.sunset = localTime(sunset) ?? sevenPM
+        self.solarNoon = localTime(solarNoon) ?? noon
         dayLength = UInt64(defaults.integer(forKey: "day_length"))
-        civilSunrise = localTime(civilTwilightBegin)
-        civilSunset = localTime(civilTwilightEnd)
-        nauticalSunrise = localTime(nauticalTwilightBegin)
-        nauticalSunset = localTime(nauticalTwilightEnd)
-        astronomicalSunrise = localTime(astronomicalTwilightBegin)
-        astronomicalSunset = localTime(astronomicalTwilightEnd)
+        civilSunrise = localTime(civilTwilightBegin) ?? sevenAM
+        civilSunset = localTime(civilTwilightEnd) ?? sevenPM
+        nauticalSunrise = localTime(nauticalTwilightBegin) ?? sevenAM
+        nauticalSunset = localTime(nauticalTwilightEnd) ?? sevenPM
+        astronomicalSunrise = localTime(astronomicalTwilightBegin) ?? sevenAM
+        astronomicalSunset = localTime(astronomicalTwilightEnd) ?? sevenPM
     }
 
     func store() {
-        datastore.defaults.set(sunrise.iso8601(), forKey: "sunrise")
-        datastore.defaults.set(sunset.iso8601(), forKey: "sunset")
-        datastore.defaults.set(solarNoon.iso8601(), forKey: "solar_noon")
+        datastore.defaults.set(sunrise.toISO(), forKey: "sunrise")
+        datastore.defaults.set(sunset.toISO(), forKey: "sunset")
+        datastore.defaults.set(solarNoon.toISO(), forKey: "solar_noon")
         datastore.defaults.set(dayLength, forKey: "day_length")
-        datastore.defaults.set(civilSunrise.iso8601(), forKey: "civil_twilight_begin")
-        datastore.defaults.set(civilSunset.iso8601(), forKey: "civil_twilight_end")
-        datastore.defaults.set(nauticalSunrise.iso8601(), forKey: "nautical_twilight_begin")
-        datastore.defaults.set(nauticalSunset.iso8601(), forKey: "nautical_twilight_end")
-        datastore.defaults.set(astronomicalSunrise.iso8601(), forKey: "astronomical_twilight_begin")
-        datastore.defaults.set(astronomicalSunset.iso8601(), forKey: "astronomical_twilight_end")
+        datastore.defaults.set(civilSunrise.toISO(), forKey: "civil_twilight_begin")
+        datastore.defaults.set(civilSunset.toISO(), forKey: "civil_twilight_end")
+        datastore.defaults.set(nauticalSunrise.toISO(), forKey: "nautical_twilight_begin")
+        datastore.defaults.set(nauticalSunset.toISO(), forKey: "nautical_twilight_end")
+        datastore.defaults.set(astronomicalSunrise.toISO(), forKey: "astronomical_twilight_begin")
+        datastore.defaults.set(astronomicalSunset.toISO(), forKey: "astronomical_twilight_end")
     }
 
     // MARK: NSCoding
 
     func encode(with aCoder: NSCoder) {
-        aCoder.encode(sunrise.iso8601(), forKey: "sunrise")
-        aCoder.encode(sunset.iso8601(), forKey: "sunset")
-        aCoder.encode(solarNoon.iso8601(), forKey: "solar_noon")
+        aCoder.encode(sunrise.toISO(), forKey: "sunrise")
+        aCoder.encode(sunset.toISO(), forKey: "sunset")
+        aCoder.encode(solarNoon.toISO(), forKey: "solar_noon")
         aCoder.encode(dayLength, forKey: "day_length")
-        aCoder.encode(civilSunrise.iso8601(), forKey: "civil_twilight_begin")
-        aCoder.encode(civilSunset.iso8601(), forKey: "civil_twilight_end")
-        aCoder.encode(nauticalSunrise.iso8601(), forKey: "nautical_twilight_begin")
-        aCoder.encode(nauticalSunset.iso8601(), forKey: "nautical_twilight_end")
-        aCoder.encode(astronomicalSunrise.iso8601(), forKey: "astronomical_twilight_begin")
-        aCoder.encode(astronomicalSunset.iso8601(), forKey: "astronomical_twilight_end")
+        aCoder.encode(civilSunrise.toISO(), forKey: "civil_twilight_begin")
+        aCoder.encode(civilSunset.toISO(), forKey: "civil_twilight_end")
+        aCoder.encode(nauticalSunrise.toISO(), forKey: "nautical_twilight_begin")
+        aCoder.encode(nauticalSunset.toISO(), forKey: "nautical_twilight_end")
+        aCoder.encode(astronomicalSunrise.toISO(), forKey: "astronomical_twilight_begin")
+        aCoder.encode(astronomicalSunset.toISO(), forKey: "astronomical_twilight_end")
     }
 
     required init?(coder aDecoder: NSCoder) {
-        let localTime = { (iso: String) in DateInRegion(
-            string: iso,
-            format: .iso8601Auto
-        )!.toRegion(Region.Local())
+        let (sevenAM, noon, sevenPM) = Moment.defaultMoments()
+
+        let localTime = { (iso: String) -> DateInRegion? in
+            iso.toDate()?.convertTo(region: Region.local)
         }
 
         guard let sunrise = aDecoder.decodeObject(forKey: "sunrise") as? String,
@@ -144,16 +160,16 @@ class Moment: NSObject, NSCoding {
             return nil
         }
 
-        self.sunrise = localTime(sunrise)
-        self.sunset = localTime(sunset)
-        self.solarNoon = localTime(solarNoon)
+        self.sunrise = localTime(sunrise) ?? sevenAM
+        self.sunset = localTime(sunset) ?? sevenPM
+        self.solarNoon = localTime(solarNoon) ?? noon
         dayLength = UInt64(aDecoder.decodeInt64(forKey: "day_length"))
-        civilSunrise = localTime(civilTwilightBegin)
-        civilSunset = localTime(civilTwilightEnd)
-        nauticalSunrise = localTime(nauticalTwilightBegin)
-        nauticalSunset = localTime(nauticalTwilightEnd)
-        astronomicalSunrise = localTime(astronomicalTwilightBegin)
-        astronomicalSunset = localTime(astronomicalTwilightEnd)
+        civilSunrise = localTime(civilTwilightBegin) ?? sevenAM
+        civilSunset = localTime(civilTwilightEnd) ?? sevenPM
+        nauticalSunrise = localTime(nauticalTwilightBegin) ?? sevenAM
+        nauticalSunset = localTime(nauticalTwilightEnd) ?? sevenPM
+        astronomicalSunrise = localTime(astronomicalTwilightBegin) ?? sevenAM
+        astronomicalSunset = localTime(astronomicalTwilightEnd) ?? sevenPM
     }
 
     func serialize() {
