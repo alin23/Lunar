786a6a7394414de9b78bf55de51205ed04ec0763
Alin Panaitiu <alin.p32@gmail.com>
2018-10-03 23:34:18 +0300

    Fix minReplyDelay

--- Lunar/DDC.c
+++ Lunar/DDC.c
@@ -12,7 +12,7 @@
 #define kMaxRequests 10
 
 /*
- 
+
  Iterate IOreg's device tree to find the IOFramebuffer mach service port that corresponds to a given CGDisplayID
  replaces CGDisplayIOServicePort: https://developer.apple.com/library/mac/documentation/GraphicsImaging/Reference/Quartz_Services_Ref/index.html#//apple_ref/c/func/CGDisplayIOServicePort
  based on: https://github.com/glfw/glfw/pull/192/files
@@ -21,14 +21,14 @@ static io_service_t IOFramebufferPortFromCGDisplayID(CGDirectDisplayID displayID
 {
     io_iterator_t iter;
     io_service_t serv, servicePort = 0;
-    
+
     kern_return_t err = IOServiceGetMatchingServices(kIOMasterPortDefault, IOServiceMatching(IOFRAMEBUFFER_CONFORMSTO), &iter);
-    
+
     if (err != KERN_SUCCESS) {
         IOObjectRelease(iter);
         return 0;
     }
-    
+
     // now recurse the IOReg tree
     while ((serv = IOIteratorNext(iter)) != MACH_PORT_NULL)
     {
@@ -37,20 +37,20 @@ static io_service_t IOFramebufferPortFromCGDisplayID(CGDirectDisplayID displayID
         CFIndex vendorID = 0, productID = 0, serialNumber = 0;
         CFNumberRef vendorIDRef, productIDRef, serialNumberRef;
         Boolean success = 0;
-        
+
         // get metadata from IOreg node
         IORegistryEntryGetName(serv, name);
         info = IODisplayCreateInfoDictionary(serv, kIODisplayOnlyPreferredName);
-        
+
         if (CFDictionaryGetValueIfPresent(info, CFSTR(kDisplayVendorID), (const void**)&vendorIDRef))
             success = CFNumberGetValue(vendorIDRef, kCFNumberCFIndexType, &vendorID);
-        
+
         if (CFDictionaryGetValueIfPresent(info, CFSTR(kDisplayProductID), (const void**)&productIDRef))
             success &= CFNumberGetValue(productIDRef, kCFNumberCFIndexType, &productID);
-        
+
         IOItemCount busCount;
         IOFBGetI2CInterfaceCount(serv, &busCount);
-        
+
         if (!success || busCount < 1 || CGDisplayIsBuiltin(displayID)) {
             // this does not seem to be a DDC-enabled display, skip it
             CFRelease(info);
@@ -58,10 +58,10 @@ static io_service_t IOFramebufferPortFromCGDisplayID(CGDirectDisplayID displayID
         }
         // if (framebuffer.hasDDCConnect(0)) // https://developer.apple.com/reference/kernel/ioframebuffer/1813510-hasddcconnect?language=objc
         // kAppleDisplayTypeKey -- if this is an Apple display, can use IODisplay func to change brightness: http://stackoverflow.com/a/32691700/3878712
-        
+
         if (CFDictionaryGetValueIfPresent(info, CFSTR(kDisplaySerialNumber), (const void**)&serialNumberRef))
             CFNumberGetValue(serialNumberRef, kCFNumberCFIndexType, &serialNumber);
-        
+
         // compare IOreg's metadata to CGDisplay's metadata to infer if the IOReg's I2C monitor is the display for the given NSScreen.displayID
         if (CGDisplayVendorNumber(displayID) != vendorID  ||
             CGDisplayModelNumber(displayID)  != productID ||
@@ -75,7 +75,7 @@ static io_service_t IOFramebufferPortFromCGDisplayID(CGDirectDisplayID displayID
         CFRelease(info);
         break;
     }
-    
+
     IOObjectRelease(iter);
     return servicePort;
 }
@@ -167,14 +167,14 @@ bool DDCRead(CGDirectDisplayID displayID, struct DDCReadCommand *read) {
 
     for (int i=1; i<=kMaxRequests; i++) {
         bzero(&request, sizeof(request));
-        
-        request.commFlags                       = 0;   
+
+        request.commFlags                       = 0;
         request.sendAddress                     = 0x6E;
         request.sendTransactionType             = kIOI2CSimpleTransactionType;
         request.sendBuffer                      = (vm_address_t) &data[0];
         request.sendBytes                       = 5;
-        request.minReplyDelay                   = 10;  // too short can freeze kernel
-        
+        request.minReplyDelay                   = 30 * kMillisecondScale;
+
         data[0] = 0x51;
         data[1] = 0x82;
         data[2] = 0x01;
@@ -189,13 +189,13 @@ bool DDCRead(CGDirectDisplayID displayID, struct DDCReadCommand *read) {
 #endif
         request.replyAddress            = 0x6F;
         request.replySubAddress         = 0x51;
-        
+
         request.replyBuffer = (vm_address_t) reply_data;
         request.replyBytes = sizeof(reply_data);
-        
+
         result = DisplayRequest(displayID, &request);
         result = (result && reply_data[0] == request.sendAddress && reply_data[2] == 0x2 && reply_data[4] == read->control_id && reply_data[10] == (request.replyAddress ^ request.replySubAddress ^ reply_data[1] ^ reply_data[2] ^ reply_data[3] ^ reply_data[4] ^ reply_data[5] ^ reply_data[6] ^ reply_data[7] ^ reply_data[8] ^ reply_data[9]));
-    
+
         if (result) { // checksum is ok
             if (i > 1) {
                 printf("D: Tries required to get data: %d \n", i);
@@ -205,7 +205,7 @@ bool DDCRead(CGDirectDisplayID displayID, struct DDCReadCommand *read) {
 
         if (request.result == kIOReturnUnsupportedMode)
             printf("E: Unsupported Transaction Type! \n");
-        
+
         // reset values and return 0, if data reading fails
         if (i >= kMaxRequests) {
             read->success = false;
@@ -214,7 +214,7 @@ bool DDCRead(CGDirectDisplayID displayID, struct DDCReadCommand *read) {
             printf("E: No data after %d tries! \n", i);
             return 0;
         }
-        
+
         usleep(40000); // 40msec -> See DDC/CI Vesa Standard - 4.4.1 Communication Error Recovery
     }
     read->success = true;
@@ -234,29 +234,29 @@ UInt32 SupportedTransactionType() {
     kern_return_t   kr;
     io_iterator_t   io_objects;
     io_service_t    io_service;
-    
+
     kr = IOServiceGetMatchingServices(kIOMasterPortDefault,
                                       IOServiceNameMatching("IOFramebufferI2CInterface"), &io_objects);
-    
+
     if (kr != KERN_SUCCESS) {
         printf("E: Fatal - No matching service! \n");
         return 0;
     }
-    
+
     UInt32 supportedType = 0;
-    
+
     while((io_service = IOIteratorNext(io_objects)) != MACH_PORT_NULL)
     {
         CFMutableDictionaryRef service_properties;
         CFIndex types = 0;
         CFNumberRef typesRef;
-        
+
         kr = IORegistryEntryCreateCFProperties(io_service, &service_properties, kCFAllocatorDefault, kNilOptions);
         if (kr == KERN_SUCCESS)
         {
             if (CFDictionaryGetValueIfPresent(service_properties, CFSTR(kIOI2CTransactionTypesKey), (const void**)&typesRef))
                 CFNumberGetValue(typesRef, kCFNumberCFIndexType, &types);
-            
+
             /*
              We want DDCciReply but Simple is better than No-thing.
              Combined and DisplayPortNative are not useful in our case.
@@ -264,7 +264,7 @@ UInt32 SupportedTransactionType() {
             if (types) {
 #ifdef DEBUG
                 printf("\nD: IOI2CTransactionTypes: 0x%02lx (%ld)\n", types, types);
-                
+
                 // kIOI2CNoTransactionType = 0
                 if ( 0 == ((1 << kIOI2CNoTransactionType) & (UInt64)types)) {
                     printf("E: IOI2CNoTransactionType                   unsupported \n");
@@ -280,7 +280,7 @@ UInt32 SupportedTransactionType() {
                     printf("D: IOI2CSimpleTransactionType               supported \n");
                     supportedType = kIOI2CSimpleTransactionType;
                 }
-                
+
                 // kIOI2CDDCciReplyTransactionType = 2
                 if ( 0 == ((1 << kIOI2CDDCciReplyTransactionType) & (UInt64)types)) {
                     printf("E: IOI2CDDCciReplyTransactionType           unsupported \n");
@@ -296,7 +296,7 @@ UInt32 SupportedTransactionType() {
                     printf("D: IOI2CCombinedTransactionType             supported \n");
                     //supportedType = kIOI2CCombinedTransactionType;
                 }
-                
+
                 // kIOI2CDisplayPortNativeTransactionType = 4
                 if ( 0 == ((1 << kIOI2CDisplayPortNativeTransactionType) & (UInt64)types)) {
                     printf("E: IOI2CDisplayPortNativeTransactionType    unsupported\n");
@@ -311,23 +311,23 @@ UInt32 SupportedTransactionType() {
                 if ( 0 != ((1 << kIOI2CSimpleTransactionType) & (UInt64)types)) {
                     supportedType = kIOI2CSimpleTransactionType;
                 }
-                
+
                 // kIOI2CDDCciReplyTransactionType = 2
                 if ( 0 != ((1 << kIOI2CDDCciReplyTransactionType) & (UInt64)types)) {
                     supportedType = kIOI2CDDCciReplyTransactionType;
                 }
 #endif
             } else printf("E: Fatal - No supported Transaction Types! \n");
-            
+
             CFRelease(service_properties);
         }
 
         IOObjectRelease(io_service);
-        
+
         // Mac OS offers three framebuffer devices, but we can leave here
         if (supportedType > 0) return supportedType;
     }
-    
+
     return supportedType;
 }
 
