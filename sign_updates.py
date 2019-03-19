d45980b7b665965bbb678f24aad0b8a7fb0fbb1b
Alin Panaitiu <alin.panaitiu@gmail.com>
2019-03-19 18:42:50 +0200

    Improve dev experience with some Makefile automation

--- sign_updates.py
+++ sign_updates.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 import os
 import re
-import sys
 import subprocess
+import sys
 from pathlib import Path
 
-from lxml import etree
+from lxml import etree, html
 from lxml.builder import ElementMaker
+from markdown2 import markdown_path
 
-appcast_path = Path(sys.argv[1])
-key_path = Path(sys.argv[2])
+key_path = Path(sys.argv[1])
+release_notes = Path.cwd() / "ReleaseNotes"
+appcast_path = Path.cwd() / "Releases" / "appcast.xml"
 
 parser = etree.XMLParser(strip_cdata=False)
 appcast = etree.parse(str(appcast_path), parser=parser)
@@ -19,6 +21,36 @@ DELTA_PATTERN = re.compile(
     r"Lunar([0-9]+\.[0-9]+\.[0-9]+)-([0-9]+\.[0-9]+\.[0-9]+).delta"
 )
 E = ElementMaker(nsmap={"sparkle": SPARKLE})
+H = ElementMaker()
+
+CHANGELOG_STYLE = H.style(
+    """
+    * {
+        font-family: Menlo, monospace;
+        color: #333333;
+    }
+
+    a {
+        color: #1DA1F2;
+    }
+
+    a:hover {
+        color: #0077b5;
+    }
+
+    h2#features {
+        color: #EDB44B;
+    }
+
+    h2#improvements {
+        color: #3E3E70;
+    }
+
+    h2#fixes {
+        color: #E14283;
+    }
+"""
+)
 
 
 def sparkle(attr):
@@ -34,18 +66,35 @@ def get_signature(file):
 
 
 for item in appcast.iter("item"):
-    delta_enclosures = []
     enclosure = item.find("enclosure")
+    description = item.find("description")
+
     url = enclosure.attrib["url"]
     version = enclosure.attrib[sparkle("version")]
-    dmg = appcast_path.with_name(os.path.basename(url))
-    signature = get_signature(dmg)
-    enclosure.set(sparkle("dsaSignature"), signature)
     enclosure.set("url", f"https://lunarapp.site/download/{version}")
+
+    dmg = appcast_path.with_name(f"Lunar-{version}.dmg")
+    enclosure.set(sparkle("dsaSignature"), get_signature(dmg))
+
+    release_notes_file = release_notes / f"{version}.md"
+    if description is None and release_notes_file.exists():
+
+        changelog = html.fromstring(
+            markdown_path(str(release_notes_file), extras=["header-ids"])
+        )
+        description = E.description(
+            etree.CDATA(
+                html.tostring(
+                    H.div(CHANGELOG_STYLE, changelog), encoding="unicode"
+                ).replace("\n", "")
+            )
+        )
+        item.append(description)
 
     for delta in item.findall(sparkle("deltas")):
         item.remove(delta)
 
+    # delta_enclosures = []
     # for delta in appcast_path.parent.glob(f"Lunar{version}-*.delta"):
     #     new_version, old_version = DELTA_PATTERN.match(delta.name).groups()
     #     enclosure = E.enclosure(
