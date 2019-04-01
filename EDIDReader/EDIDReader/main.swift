3b30d5931e505a74d441d0d1f323bd26f87cb23d
Alin Panaitiu <alin.panaitiu@gmail.com>
2019-04-01 18:46:49 +0300

    Try to extract EDID data correctly

--- EDIDReader/EDIDReader/main.swift
+++ EDIDReader/EDIDReader/main.swift
@@ -8,6 +8,48 @@
 
 import Foundation
 
-DDC.findExternalDisplays().forEach {
-    DDC.printTextDescriptors(displayID: $0)
+func printEdid(_ data: Data) {
+    let edid = data.map { $0 }.str(length: 128)
+    let id = data[77 ..< 90].map { $0 }.str(length: 13)
+    print("      EDID: \(edid)")
+    print("      ID: \(id)\n")
+}
+
+func printDetails(_ id: CGDirectDisplayID) {
+    let sn = CGDisplaySerialNumber(id)
+    let model = CGDisplayModelNumber(id)
+    let vendor = CGDisplayVendorNumber(id)
+
+    print("    S/N: \(sn.str())")
+    print("    Model Number: \(model.str())")
+    print("    Vendor: \(vendor.str())\n")
+
+    let idData = DDC.getDisplayIdentificationData(displayID: id)
+    let name = DDC.getDisplayName(for: id)
+    let serial = DDC.getDisplaySerial(for: id)
+
+    print("    Name: \(name)")
+    print("    Serial: \(serial)\n")
+
+    print("    Display unique ID: \(idData)\n")
+
+    if let data = DDC.getEdidData(displayID: id) {
+        print("    Raw data (guess match):")
+        printEdid(data)
+    }
+}
+
+if let id = DDC.getBuiltinDisplay() {
+    print("Built-in display:")
+    printDetails(id)
+}
+
+DDC.findExternalDisplays().forEach { id in
+    print("External displays:")
+    printDetails(id)
+}
+
+print("Raw data:")
+DDC.getEdidData().forEach { data in
+    printEdid(data)
 }
