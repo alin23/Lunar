6dd63f85b9f3574cf9028068d56ae3c96d252cf9
Alin Panaitiu <alin.panaitiu@gmail.com>
2019-04-20 00:34:56 +0300

    Add more diagnostics tools

--- gencode.sh
+++ gencode.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 if [ -f ./.env.sh ]; then
     source ./.env.sh
 fi
 sourcery
+defaults write $DIR/Lunar/Info.plist Fabric -dict-add APIKey $FABRIC_API_KEY
