40bb0cb5a98a72fd9c0407cb9293b74608f08e4d
Alin Panaitiu <alin.panaitiu@gmail.com>
2019-10-23 17:36:32 +0300

    Prepare Lunar for notarization

--- buildscript.sh
+++ buildscript.sh
@@ -3,4 +3,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 if [ -f $DIR/.env.sh ]; then
     source $DIR/.env.sh
 fi
-test "$FABRIC_BUILD_SECRET" && $DIR/Fabric.framework/run $FABRIC_API_KEY $FABRIC_BUILD_SECRET || true
+test "$FABRIC_BUILD_SECRET" && $DIR/Frameworks/Fabric.framework/run $FABRIC_API_KEY $FABRIC_BUILD_SECRET || true
