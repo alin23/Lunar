67afdd79de3033b8b52344f6d66f067131ec2b07
Alin Panaitiu <alin.panaitiu@gmail.com>
2019-10-24 00:08:51 +0300

    Release 2.9.0

--- Lunar/DDC/Lunar-Bridging-Header.h
+++ Lunar/DDC/Lunar-Bridging-Header.h
@@ -3,3 +3,7 @@
 //
 
 #import "DDC.h"
+#import <CoreGraphics/CoreGraphics.h>
+
+double CoreDisplay_Display_GetUserBrightness(CGDirectDisplayID display);
+void CoreDisplay_Display_SetUserBrightness(CGDirectDisplayID display, double brightness);
