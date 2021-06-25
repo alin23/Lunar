43a732fd0d27f5418421202fa0123b683df3d7b8
Alin Panaitiu <alin.panaitiu@gmail.com>
2021-06-25 15:05:29 +0300

    Add a way to adjust update check interval

--- Lunar/Data/Transformers.swift
+++ Lunar/Data/Transformers.swift
@@ -38,7 +38,42 @@ class DisplayTransformer: ValueTransformer {
     }
 }
 
+class UpdateCheckIntervalTransformer: ValueTransformer {
+    static let WEEKLY = 604_800
+    static let DAILY = 86400
+    static let HOURLY = 3600
+
+    override class func transformedValueClass() -> AnyClass {
+        NSNumber.self
+    }
+
+    override class func allowsReverseTransformation() -> Bool {
+        true
+    }
+
+    override func reverseTransformedValue(_ value: Any?) -> Any? {
+        guard let segmentIndex = value as? Int, segmentIndex == 0 || segmentIndex == 1 || segmentIndex == 2 else { return Self.DAILY }
+        switch segmentIndex {
+        case 0: return Self.HOURLY.ns
+        case 1: return Self.DAILY.ns
+        case 2: return Self.WEEKLY.ns
+        default: return Self.DAILY.ns
+        }
+    }
+
+    override func transformedValue(_ value: Any?) -> Any? {
+        guard let checkInterval = value as? Int else { return 1 }
+        switch checkInterval {
+        case 0 ... Self.HOURLY: return 0
+        case (Self.HOURLY + 1) ... Self.DAILY: return 1
+        case (Self.DAILY + 1) ... Self.WEEKLY: return 2
+        default: return 2
+        }
+    }
+}
+
 extension NSValueTransformerName {
     static let appExceptionTransformerName = NSValueTransformerName(rawValue: "AppExceptionTransformer")
     static let displayTransformerName = NSValueTransformerName(rawValue: "DisplayTransformer")
+    static let updateCheckIntervalTransformerName = NSValueTransformerName(rawValue: "UpdateCheckIntervalTransformer")
 }
