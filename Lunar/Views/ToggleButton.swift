93705357c124dd9e586749152f7d610aa2665876
Alin Panaitiu <alin.panaitiu@gmail.com>
2021-07-27 22:21:47 +0300

    Highlight Advanced settings button on first run

--- Lunar/Views/ToggleButton.swift
+++ Lunar/Views/ToggleButton.swift
@@ -36,6 +36,7 @@ class ToggleButton: NSButton {
     var hoverState = HoverState.noHover
     var bgColor: NSColor {
         if !isEnabled {
+            if highlighterTask != nil { stopHighlighting() }
             return (offStateButtonColor[hoverState]![page] ?? offStateButtonColor[hoverState]![.display]!).shadow(withLevel: 0.3)!
         } else if state == .on {
             return onStateButtonColor[hoverState]![page] ?? onStateButtonColor[hoverState]![.display]!
@@ -52,6 +53,8 @@ class ToggleButton: NSButton {
         }
     }
 
+    weak var notice: NSTextField?
+
     override init(frame frameRect: NSRect) {
         super.init(frame: frameRect)
         setup()
@@ -62,9 +65,73 @@ class ToggleButton: NSButton {
         setup()
     }
 
+    @AtomicLock var highlighterTask: CFRunLoopTimer?
+
+    func highlight() {
+        guard !isHidden else { return }
+
+        let windowVisible = mainThread { window?.isVisible ?? false }
+
+        guard highlighterTask == nil || !realtimeQueue.isValid(timer: highlighterTask!), windowVisible
+        else {
+            return
+        }
+
+        highlighterTask = realtimeQueue.async(every: 5.seconds) { [weak self] (_: CFRunLoopTimer?) in
+            guard let s = self else {
+                if let timer = self?.highlighterTask { realtimeQueue.cancel(timer: timer) }
+                return
+            }
+
+            let windowVisible: Bool = mainThread { s.window?.isVisible ?? false }
+            guard windowVisible, let notice = s.notice else {
+                if let timer = self?.highlighterTask { realtimeQueue.cancel(timer: timer) }
+                return
+            }
+
+            mainThread {
+                if notice.alphaValue <= 0.02 {
+                    notice.transition(1)
+                    notice.alphaValue = 0.9
+                    notice.needsDisplay = true
+
+                    s.hover(fadeDuration: 1)
+                    s.needsDisplay = true
+                } else {
+                    notice.transition(3)
+                    notice.alphaValue = 0.01
+                    notice.needsDisplay = true
+
+                    s.defocus(fadeDuration: 3)
+                    s.needsDisplay = true
+                }
+            }
+        }
+    }
+
+    func stopHighlighting() {
+        if let timer = highlighterTask {
+            realtimeQueue.cancel(timer: timer)
+        }
+        highlighterTask = nil
+
+        mainThread {
+            if let notice = notice {
+                notice.transition(0.3)
+                notice.alphaValue = 0.0
+                notice.needsDisplay = true
+            }
+
+            defocus(fadeDuration: 0.3)
+            needsDisplay = true
+        }
+    }
+
     override func mouseEntered(with _: NSEvent) {
         if isEnabled {
             hover()
+        } else if highlighterTask != nil {
+            stopHighlighting()
         }
     }
 
@@ -83,14 +150,14 @@ class ToggleButton: NSButton {
         setColors()
     }
 
-    func defocus() {
+    func defocus(fadeDuration: TimeInterval = 0.2) {
         hoverState = .noHover
-        setColors()
+        setColors(fadeDuration: fadeDuration)
     }
 
-    func hover() {
+    func hover(fadeDuration: TimeInterval = 0.1) {
         hoverState = .hover
-        setColors(fadeDuration: 0.1)
+        setColors(fadeDuration: fadeDuration)
     }
 
     func setup() {
