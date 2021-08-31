b06be20856d1f4d1747def03e42636788ac0e225
Alin Panaitiu <alin.panaitiu@gmail.com>
2021-08-31 21:40:12 +0300

    Hover effects for navigation

---
+++ Lunar/Views/PageNavigationControl.swift
@@ -0,0 +1,73 @@
+//
+//  PageNavigationControl.swift
+//  PageNavigationControl
+//
+//  Created by Alin Panaitiu on 31.08.2021.
+//  Copyright © 2021 Alin. All rights reserved.
+//
+
+import Cocoa
+
+class PageNavigationControl: NSSegmentedControl {
+    // MARK: Lifecycle
+
+    override init(frame frameRect: NSRect) {
+        super.init(frame: frameRect)
+        setup()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+
+    // MARK: Internal
+
+    var trackingArea: NSTrackingArea!
+    var buttonShadow: NSShadow!
+    weak var notice: NSTextField?
+
+    var onMouseEnter: (() -> Void)?
+    var onMouseExit: (() -> Void)?
+
+    var standardAlpha = 0.5
+    var visibleAlpha = 0.8
+
+    func setup() {
+        wantsLayer = true
+
+        alphaValue = standardAlpha
+
+        trackingArea = NSTrackingArea(
+            rect: visibleRect,
+            options: [.mouseEnteredAndExited, .activeInActiveApp],
+            owner: self,
+            userInfo: nil
+        )
+        addTrackingArea(trackingArea)
+    }
+
+    override func mouseEntered(with _: NSEvent) {
+        if !isEnabled { return }
+
+        transition(0.1)
+        alphaValue = visibleAlpha
+
+        onMouseEnter?()
+    }
+
+    override func mouseExited(with _: NSEvent) {
+        if !isEnabled { return }
+
+        transition(0.2)
+        alphaValue = standardAlpha
+
+        onMouseExit?()
+    }
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+}
