15d2eab9d7dcd8a49418f209f262465309fbe8dc
Alin Panaitiu <alin.panaitiu@gmail.com>
2022-08-13 22:16:46 +0300

    Update sensor installer

--- Lunar/ALS/install.sh
+++ Lunar/ALS/install.sh
@@ -11,23 +11,23 @@ export PATH="/opt/homebrew/bin:/usr/local/bin:$PATH"
 LOG_PATH=${LOG_PATH:-/tmp/lunar-sensor-install.log}
 BOARD="${BOARD:-esp32dev}"
 
-if [[ "$BOARD" == "metroesp32-s2" || "$BOARD" == "featheresp32-s2" ]]; then
-    PLATFORM_VERSION="${PLATFORM_VERSION:-https://static.lunar.fyi/platform-espressif32-b331f75383faaa7bc8ee971b0cdb83177feb8284.zip}"
+if [[ "$BOARD" == "adafruit_metro_esp32s2" || "$BOARD" == "adafruit_funhouse_esp32s2"  || "$BOARD" == "adafruit_feather_esp32s2_tft"  || "$BOARD" == "adafruit_magtag29_esp32s2" ]]; then
+    PLATFORM_VERSION="${PLATFORM_VERSION:-platformio/espressif32@5.1.0}"
     PLATFORM="${PLATFORM:-ESP32}"
     SDA="${SDA:-33}"
     SCL="${SCL:-34}"
 elif [[ "$BOARD" == "nodemcuv2" || "$BOARD" == "d1_mini" || "$BOARD" == "d1_mini_lite" || "$BOARD" == "d1_mini_pro" || "$BOARD" == "nodemcu" ]]; then
-    PLATFORM_VERSION="${PLATFORM_VERSION:-platformio/espressif8266@3.1.0}"
+    PLATFORM_VERSION="${PLATFORM_VERSION:-platformio/espressif8266@4.0.1}"
     PLATFORM="${PLATFORM:-ESP8266}"
     SDA="${SDA:-D2}"
     SCL="${SCL:-D1}"
 elif [[ "$BOARD" == "esp32dev" || "$BOARD" == "lolin32" || "$BOARD" == "lolin32_lite" || "$BOARD" == "nodemcu-32s" ]]; then
-    PLATFORM_VERSION="${PLATFORM_VERSION:-platformio/espressif32@3.3.1}"
+    PLATFORM_VERSION="${PLATFORM_VERSION:-platformio/espressif32@5.1.0}"
     PLATFORM="${PLATFORM:-ESP32}"
     SDA="${SDA:-19}"
     SCL="${SCL:-23}"
 else
-    PLATFORM_VERSION="${PLATFORM_VERSION:-platformio/espressif8266@3.1.0}"
+    PLATFORM_VERSION="${PLATFORM_VERSION:-platformio/espressif8266@4.0.1}"
     PLATFORM="${PLATFORM:-ESP8266}"
     SDA="${SDA:-GPIO4}"
     SCL="${SCL:-GPIO5}"
@@ -43,7 +43,7 @@ echo "" | tee -a "$LOG_PATH"
 echo "Installing ESPHome..." | tee -a "$LOG_PATH"
 echo "" | tee -a "$LOG_PATH"
 echo "" | tee -a "$LOG_PATH"
-/usr/bin/python3 -m pip install --user --no-cache https://static.lunar.fyi/esphome-dev.zip 2>&1 | tee -a "$LOG_PATH"
+/usr/bin/python3 -m pip install --user --no-cache git+https://github.com/alin23/esphome@dev#egg=esphome 2>&1 | tee -a "$LOG_PATH"
 
 echo "" | tee -a "$LOG_PATH"
 echo "Compiling and uploading firmware to $ESP_DEVICE ($BOARD) ..." | tee -a "$LOG_PATH"
